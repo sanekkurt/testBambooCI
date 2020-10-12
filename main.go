012f2ddf3a78464f5734bcfb62bfe6e057092331
sanekkurt <sanek_kurt@mail.ru>
2020-10-12 13:34:52 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func service() {
+	fmt.Println("Hello from service!")
+}
+
+func main() {
+	fmt.Println("main() started")
+
+	go service()
+	time.Sleep(1 * time.Second)
+
+
+	fmt.Println("main() stopped")
+}
